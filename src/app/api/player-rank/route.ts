2e119b7099c6467a088f91dad7ba916fce682d6e
Jan Kuta <jan.kuta@email.cz>
2025-05-20 17:09:05 +0200

    Updated player-rank query

--- src/app/api/player-rank/route.ts
+++ src/app/api/player-rank/route.ts
@@ -6,16 +6,15 @@ export async function POST(req: Request) {
 
   try {
     const { rows } = await pool.sql`
-  SELECT rank, steam_id, name, top_score, updated_at FROM (
     SELECT
         steam_id,
         name,
         top_score,
         updated_at,
-        RANK() OVER (ORDER BY top_score DESC)  AS rank
+        RANK() OVER (ORDER BY top_score DESC)  AS rank,
+        COUNT(*) OVER () AS total_players
     FROM leaderboard_entries
-  ) ranked
-  WHERE steam_id = ${steam_id};
+    WHERE steam_id = ${steam_id};
     `
 
     return NextResponse.json(rows)
